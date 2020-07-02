387801783cf5f75a8a1fa43f126d00c5ae5d37cc
Shawn C <scooter4356@gmail.com>
2020-07-02 10:33:57 -0500

    Add project files.

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldAsp.Net.Controllers
+{
+    public class HelloController : Controller
+    {
+        [HttpGet]
+        [Route("/helloshawn/")]
+        public IActionResult Index()
+        {
+            string html = "<h1> Hello World!!!</h1>";
+            return Content(html, "text/html");
+        }
+
+        [HttpGet]
+        [Route("/hello/welcome/")]
+        public IActionResult Welcome(string name)
+        {
+            return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
+        }
+           
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HelloWorldAsp.Net.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
