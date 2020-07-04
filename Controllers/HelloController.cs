52b82c6890e2b2d913cfd64d8d00b602547c688a
Shawn C <scooter4356@gmail.com>
2020-07-04 12:40:34 -0500

    working 13.2.2 video @ 4:06

--- Controllers/HelloController.cs
+++ Controllers/HelloController.cs
@@ -1,29 +1,33 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace HelloWorldAsp.Net.Controllers
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HelloASPDotNET.Controllers
 {
-    [Route("/helloshawn/")]
     public class HelloController : Controller
     {
+        // GET: /<controller>/
         [HttpGet]
         public IActionResult Index()
         {
-            string html = "<form method='post' action='/helloshawn/'>" + "<input type='text' name='name'/>" + "<input type='submit' value='Greet Me'/>" + "<form/>";
+            string html = "<form method='post' action='/hello'>" +
+                "<input type='text' name='name' />" +
+                "<input type='submit' value='Greet Me!' />" +
+                "</form>";
+
             return Content(html, "text/html");
         }
-        [HttpGet("welcome/{name?}")]
+
         [HttpPost]
-        public IActionResult Welcome(string name = "No Name")
+        [Route("/hello")]
+        public IActionResult Welcome(string name = "World")
         {
             return Content("<h1>Welcome to my app, " + name + "!</h1>", "text/html");
         }
-           
-
-        
-
     }
 }
