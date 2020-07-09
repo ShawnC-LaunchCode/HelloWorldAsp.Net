f7aca5105b353ab0575b132ff0fabb6fa09f09f1
Shawn C <scooter4356@gmail.com>
2020-07-09 14:14:09 -0500

    13.6.2 end of video, everything working

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HelloWorldAsp.Net.Controllers
+{
+    public class EventsController : Controller
+    {
+        static private List<string> Events = new List<string>();
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Events.Add("Strange Loop");
+            Events.Add("Grace Hopper");
+            Events.Add("Code with Pride");
+            ViewBag.events = Events;
+
+            return View();
+        }
+    }
+}
